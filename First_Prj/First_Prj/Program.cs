e567ba076e15e31b8fb84924dacd6ec28f2f4594
Arioo Barzan <arioobarzan@hotmail.com>
2023-05-20 09:54:50 +0330

    main

---
+++ First_Prj/First_Prj/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace First_Prj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int x;
+            Console.WriteLine("Plz enter a num:");
+            string number = Console.ReadLine();
+            x = int.Parse(number);
+            x *= 3;
+            Console.WriteLine(x);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
